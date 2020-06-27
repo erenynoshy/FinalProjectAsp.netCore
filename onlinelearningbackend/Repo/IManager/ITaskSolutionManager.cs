412ddd6d77492f46df97697d46b13e453a991d0a
ereny <erenykaramfci@gmail.com>
2020-06-27 19:32:31 +0200

    Add project files.

---
+++ onlinelearningbackend/Repo/IManager/ITaskSolutionManager.cs
@@ -0,0 +1,18 @@
+﻿using onlinelearningbackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onlinelearningbackend.Manager
+{
+   public interface ITaskSolutionManager
+    {
+        TaskSolution GetTaskSolutionById(int TaskSolutionId);
+       TaskSolution AddTaskByStudent( TaskSolution newTaskSolution);
+        IEnumerable<TaskSolution> GetTaskSolutionByStudentId(string StudentId, int TaskId);
+
+        IEnumerable<TaskSolution> EditTaskSolution(string StudentId, TaskSolution newTaskSolution);
+      void DeleteTaskSolutionByTaskId(int TaskSolutionId);
+    }
+}
